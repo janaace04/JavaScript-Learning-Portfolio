302389f5d1a285c3c167eeb4d7802749260fb84a
janaace04 <jana.a.c.e.04@gmail.com>
2026-09-22 17:50:27 +0530

    learn: JavaScript Introduction and variables

---
+++ 01-Basics/01-Introduction/hello.js
@@ -0,0 +1,10 @@
+console.log("Hi, Welcome JavaScript");
+
+console.log(10 + 20)
+
+let name = "Ace"
+
+let age = "23"
+
+console.log("Name: ", name);
+console.log("Age", age);
---
+++ 01-Basics/02-Variables/variable.js
@@ -0,0 +1,14 @@
+const name = "Ace";
+let age = 22;
+
+const role = "Developer";
+
+let learn = true;
+
+let ctc = "1.7 LPA"
+
+console.log("Name: ", name, "age: ", age, "Role: ", role, "Learn: ", learn, "CTC: ", ctc);
+
+ age = 23;
+// After changing age
+ console.log("Name: ", name, "age: ", age, "Role: ", role, "Learn: ", learn, "CTC: ", ctc);

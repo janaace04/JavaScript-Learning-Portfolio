009be4121e3950ebcd16cb7746a603f2934de892
janaace04 <jana.a.c.e.04@gmail.com>
2026-09-23 10:48:20 +0530

    Learn: JavaScript arithmetic operators

---
+++ 01-Basics/03-Arithmetic/arithmetic.js
@@ -0,0 +1,45 @@
+let students = 30;
+
+students = students + 1;
+console.log(students); //31
+
+students = students - 1;
+console.log(students); //30
+
+students = students * 2;
+console.log(students); //60
+
+students = students / 2;
+console.log(students); //30
+
+students = students % 4;
+console.log(students); //2
+
+students = students ** 4;
+console.log(students); //16
+
+students += 14;
+console.log(students); //30
+
+students -= 14;
+console.log(students); //16
+
+students /= 4;
+console.log(students); //4
+
+students *= 10;
+console.log(students); //40
+
+students **= 2;
+console.log(students); //1600
+
+students %= 7;
+console.log(students); //4
+
+
+
+
+
+
+
+
